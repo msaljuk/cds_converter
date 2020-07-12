a2f85b8853862eb8a874086c13a8782c6832c63e
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2020-07-12 02:57:28 -0700

    Update api.py

--- api.py
+++ api.py
@@ -24,5 +24,5 @@ def convert():
         return "<h1>" + response['status'] + "</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
+    # Threaded option to enable multiple instances for multiple user support
     app.run(threaded=True, port=5000)
